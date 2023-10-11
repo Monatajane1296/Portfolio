ba46b07354c591ee9e70c01ed1b21fe910729fb6
Monali Raju Tajane <143804182+Monatajane1296@users.noreply.github.com>
2023-10-11 11:45:12 +0530

    My website

---
+++ Index.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.anm', {
+          strings: ['SQL Developer','Web Developer','JAVA Developer'],
+          typeSpeed:100,
+          backSpeed:100,
+          backDelay:1000,    
+          loop:true
+        });
